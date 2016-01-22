938bef21093178b61545c0d1b3cc6d2b375fc4d3
Juz <juzley@gmail.com>
2016-01-22 01:01:04 +0000

    Start LCD output

--- dartsscoreboard.ino
+++ dartsscoreboard.ino
@@ -1,3 +1,6 @@
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 #include <Key.h>
 #include <Keypad.h>
 
@@ -12,6 +15,18 @@ char kpKeys[KP_ROWS][KP_COLS] = {
 byte kpRowPins[] = {8, 7, 6, 5};
 byte kpColPins[] = {4, 3, 2};
 Keypad keypad = Keypad(makeKeymap(kpKeys), kpRowPins, kpColPins, KP_ROWS, KP_COLS);
+
+#define LCD_I2C_ADDR 0x27
+#define LCD_RS_PIN 0
+#define LCD_RW_PIN 1
+#define LCD_EN_PIN 2
+#define LCD_BACKLIGHT_PIN 3
+#define LCD_D4_PIN 4
+#define LCD_D5_PIN 5
+#define LCD_D6_PIN 6
+#define LCD_D7_PIN 7
+
+LiquidCrystal_I2C lcd(LCD_I2C_ADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 enum state {
   NEW_GAME,
@@ -31,11 +46,23 @@ int scores[MAX_PLAYERS];
 
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  
   keypad.setDebounceTime(250);
   keypad.setHoldTime(2000);
   keypad.addEventListener(kpEvent);
+
+  lcd.begin(20, 4);
+
+  for(int i = 0; i< 3; i++)
+  {
+    lcd.backlight();
+    delay(250);
+    lcd.noBacklight();
+    delay(250);
+  }
+  lcd.backlight();
+    
   setupGame();
 }
 
@@ -56,8 +83,11 @@ void setupGame()
   
   numPlayers = 0;
   startingScore = 0;
-  
-  Serial.println("Enter number of players [1-8]:");
+
+  lcd.home();
+  lcd.print(" Enter # of players "); 
+  lcd.setCursor(0, 2);
+  lcd.print("       [1-8]       ");
 }
 
 void setupScoreEntry()
@@ -130,9 +160,9 @@ void kpEvent(KeypadEvent key)
         // Player number entry
         if (isDigit(key) && charToNum(key) > 1 && charToNum(key) < MAX_PLAYERS) {
           numPlayers = charToNum(key);
-          Serial.print(numPlayers);
-          Serial.println(" players");
-          Serial.println("Enter starting score, followed by #:");
+          lcd.home();
+          lcd.print("     "); lcd.print(numPlayers); lcd.print(" players");
+          lcd.print("Enter starting score, followed by #:");
         }
       } else {
         // Starting score entry
